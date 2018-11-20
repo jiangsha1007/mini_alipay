5604de494be3295b2e3e8fc523d1298ca1ff45d9
wuyaoericyy <1003181151@qq.com>
2018-11-20 16:17:49 +0800

    希望各位养成写注释的好习惯~

--- src/main/java/com/yaoqian/mini_alipay/mapper/TransactionDao.java
+++
@@ -1,9 +0,0 @@
-package com.yaoqian.mini_alipay.mapper;
-
-import com.yaoqian.mini_alipay.entity.TransactionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TransactionDao extends JpaRepository<TransactionEntity,String> {
-}

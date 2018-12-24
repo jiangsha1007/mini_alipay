d1289402ea5a144f3d34640ef4c10dc7f510ac8b
wuyaoericyy <1003181151@qq.com>
2018-12-24 15:56:38 +0800

    去除了https访问

--- src/main/java/com/yaoqian/mini_alipay/DemoApplication.java
+++ src/main/java/com/yaoqian/mini_alipay/DemoApplication.java
@@ -1,8 +1,5 @@
 package com.yaoqian.mini_alipay;
-import org.apache.catalina.Context;
-import org.apache.catalina.connector.Connector;
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,8 +7,6 @@ import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletCont
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import java.util.Properties;
 
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
 import com.github.pagehelper.PageHelper;
 import org.springframework.context.annotation.ImportResource;
@@ -59,35 +54,6 @@ public class DemoApplication implements EmbeddedServletContainerCustomizer{
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", buildConfig()); // 4
         return new CorsFilter(source);
-    }
-
-    @Bean
-    public Connector connector(){
-        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
-
-    @Bean
-    public EmbeddedServletContainerFactory servletContainerFactory(){
-        TomcatEmbeddedServletContainerFactory containerFactory = new TomcatEmbeddedServletContainerFactory(){
-
-            @Override
-            protected void postProcessContext(Context context) {
-                super.postProcessContext(context);
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
-            }
-        };
-        containerFactory.addAdditionalTomcatConnectors(connector());
-        return containerFactory;
     }
 
 }
